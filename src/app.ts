834f2b7d3d86d938868861dedd8c7050b85ae0e1
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-03-05 20:37:30 +0200

    feat(app): implement not-found handler for unsupposed routes & define app usage global error handler

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import serverConfig from './configs/server.config';
+import {
+  globalErrorHandler,
+  notFoundRouteHandler
+} from './errors/errorHandlers';
 
 const app = express();
+
+// for routes which is not supposed by our app, we use not-found router to throw an error
+app.use('*', notFoundRouteHandler);
+
+// app will use the handler as global try-catch handler
+app.use(globalErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
   //eslint-disable-next-line no-console
