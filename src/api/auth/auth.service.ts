aa278cbf7aeb70424325e9754d8926c22a8c00ef
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-03-25 20:37:48 +0200

    feat(api/auth): add logic to reassign new user tokens

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import OAuthModel from '../../dataBase/oauth.db';
 import { IOAuth } from '../../types/oauth.types';
 
-function insertTokenPair(tokenPair: IOAuth) {
-  return OAuthModel.create(tokenPair);
+async function insertTokenPair(tokenPair: IOAuth) {
+  const usersTokens = await OAuthModel.findOne({
+    _user_id: tokenPair._user_id
+  });
+
+  if (!usersTokens) {
+    return await OAuthModel.create(tokenPair);
+  }
+
+  usersTokens.accessToken = tokenPair.accessToken;
+  usersTokens.refreshToken = tokenPair.refreshToken;
+  usersTokens.save();
+
+  return usersTokens;
 }
 
 export default { insertTokenPair };
