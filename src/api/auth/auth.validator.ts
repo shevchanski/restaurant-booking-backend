9bba42d37a4fb557a12c8938ea54332bb9415724
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-03-26 14:24:49 +0200

    feat(api/auth): create token validator

--- src/api/auth/auth.validator.ts
+++ src/api/auth/auth.validator.ts
@@ -1,10 +1,41 @@
 import Joi from 'joi';
 
-import { PasswordRegex } from '../../configs/global.config';
+import {
+  PasswordRegex,
+  TokenRegex,
+  TokenTypes
+} from '../../configs/global.config';
+import APIError from '../../errors/APIError';
+import { responseStatus } from '../../configs/resStatus.config';
+import tokenService from '../../services/token.service';
 
 const loginDataValidator = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required().regex(PasswordRegex)
 });
 
-export { loginDataValidator };
+const TokenValidator = Joi.string()
+  .replace('Bearer ', '')
+  .regex(TokenRegex)
+  .required();
+
+function validateTokenDynamically(
+  token: string | undefined,
+  tokenType: TokenTypes = TokenTypes.ACCESS
+) {
+  if (!token || !token.startsWith('Bearer ')) {
+    throw new APIError('Token type is not valid', responseStatus.BAD_REQUEST);
+  }
+
+  const { error, value: validatedToken } = TokenValidator.validate(token);
+
+  if (error) {
+    throw new APIError(error.message, responseStatus.BAD_REQUEST);
+  }
+
+  tokenService.verifyToken(validatedToken, tokenType);
+
+  return validatedToken;
+}
+
+export { loginDataValidator, TokenValidator, validateTokenDynamically };
