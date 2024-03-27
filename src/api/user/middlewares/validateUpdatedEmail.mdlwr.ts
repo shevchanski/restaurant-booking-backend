0c604eb4edce46483cf421a26d8296052d16e3bc
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-03-27 15:36:04 +0200

    refactor(api/user): move email & pass validators into independent const

--- src/api/user/middlewares/validateUpdatedEmail.mdlwr.ts
+++ src/api/user/middlewares/validateUpdatedEmail.mdlwr.ts
@@ -1,15 +1,14 @@
 import { NextFunction, Response } from 'express';
 import errorWrapper from '../../../errors/errorWrapper';
 import { IRequest } from '../../../types/query.types';
-import { UpdatedEmailValidator } from '../user.validator';
+import { UpdatedUserEmailObjectValidator } from '../user.validator';
 import APIError from '../../../errors/APIError';
 import { responseStatus } from '../../../configs/resStatus.config';
 
 const validateUpdatedEmail = errorWrapper(
   (req: IRequest, res: Response, next: NextFunction) => {
-    const { error, value: validatedEmail } = UpdatedEmailValidator.validate(
-      req.body
-    );
+    const { error, value: validatedEmail } =
+      UpdatedUserEmailObjectValidator.validate(req.body);
 
     if (error) {
       throw new APIError(error.message, responseStatus.BAD_REQUEST);
--- src/api/user/user.validator.ts
+++ src/api/user/user.validator.ts
@@ -17,20 +17,24 @@ const UpdatedUserObjectValidator = joi.object<
     .regex(/^[A-Z][a-z]+/)
 });
 
+const PasswordValidator = joi.string().required().regex(PasswordRegex);
+
+const EmailValidator = joi.string().email().required().lowercase();
+
 const CreatedUserObjectValidator = UpdatedUserObjectValidator.append<
   IUser,
   IUser
 >({
-  email: joi.string().email().required().lowercase(),
-  password: joi.string().required().regex(PasswordRegex)
+  email: EmailValidator,
+  password: PasswordValidator
 });
 
-const UpdatedEmailValidator = joi.object({
-  email: joi.string().required().email()
+const UpdatedUserEmailObjectValidator = joi.object({
+  email: EmailValidator
 });
 
 export {
   UpdatedUserObjectValidator,
   CreatedUserObjectValidator,
-  UpdatedEmailValidator
+  UpdatedUserEmailObjectValidator
 };
